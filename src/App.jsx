35ce4d44ff190be77deac65a3070c78e3c0e0fbf
SenanM1 <senan.mardanli@gmail.com>
2022-12-29 13:25:08 +0400

    Fetching API

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import Movies from "./Movies";
+
 function App() {
+    const [movies, setMovies] = useState([]);
+    const [fetchError, setFetchError] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+
+    const API_URL = 'http://localhost:3500/imdb_top_1000';
+
+    useEffect(() => {
+        const fetchMovies = async () => {
+            try {
+                const res = await fetch(API_URL);
+                if(!res.ok) throw Error('The Data has not received');
+                const moviesList = await res.json();
+                setMovies(moviesList);
+                setFetchError(null);
+            } catch (e){
+                setFetchError(e.message);
+            } finally {
+                setIsLoading(false);
+            }
+        }
+
+        setTimeout(() => {
+            fetchMovies();
+        }, 2000);
+        
+    },[])
+
     return (
-        <div>
-            <h1>Hello React!</h1>
+        <div> 
+            <Movies />
+            {isLoading && <p>Loading Movies...</p>}
+            {fetchError && <p style={{color: "red"}}>{`Error: ${fetchError}`}</p>}
         </div>
     )
 };
