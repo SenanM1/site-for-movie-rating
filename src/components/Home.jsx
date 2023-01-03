023b9f54605ff82df1a2858330d79e94487c3cfc
SenanM <95751921+SenanM1@users.noreply.github.com>
2023-01-04 02:38:53 +0400

    home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,25 @@
 import "./Home.css";
+import Guys from "./guys";
+import Button from "./Button";
+import content from "./content";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="Home">
+      {content.map(
+        (content) => (
+          (<Button git={content.git} you={content.you} />),
+          (
+            <Guys
+              key={content.id}
+              name={content.name}
+              mail={content.mail}
+              image={content.image}
+              info={content.info}
+            />
+          )
+        )
+      )}
+    </div>
+  );
 }
