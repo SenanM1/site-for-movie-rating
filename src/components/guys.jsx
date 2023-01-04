e20398adf69eaa55064300d498c635543637e61a
AtillaTegin <100026159+AtillaTegin@users.noreply.github.com>
2023-01-04 06:33:43 +0400

    Update guys.jsx

--- src/components/guys.jsx
+++ src/components/guys.jsx
@@ -3,27 +3,23 @@ import "./guys.css";
 
 export default function Guys(props) {
   return (
-    <div className="container">
+   <div className="container">
       <div key={props.id} className="card">
         <div className="profile">
           <img
             src={props.image}
             alt="cardImg"
             className="cardImage"
-            width="50px"
-            height="50px"
+            width="200" 
+            height="200"
           />
-          <h4>
-            Name: {props.name} <br />
-            Mail: {props.mail}
-          </h4>
         </div>
-        <div className="description">
-          <p>{props.info}</p>
-          <div className="buttons">
-            <Button />
-          </div>
-        </div>
+        <h4 className="about">
+          Name: {props.name} <br />
+          Mail: {props.mail}
+        </h4>
+        <p className="lorem">{props.info}</p>
+        <Button />
       </div>
     </div>
   );
